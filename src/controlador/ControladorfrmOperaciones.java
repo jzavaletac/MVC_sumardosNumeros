30417fbf3876550f447f48c8ce010a1cb6751744
OCA-UIE <OCA-UIE@DESKTOP-AHIE2HK>
2024-09-23 09:49:34 -0500

    Primer Commit

--- src/controlador/ControladorfrmOperaciones.java
+++ src/controlador/ControladorfrmOperaciones.java
@@ -14,9 +14,33 @@ public class ControladorfrmOperaciones {
         this.vista = vista;
         this.modelo = modelo;
         
-        this.vista
+        this.vista.btnSumar.addActionListener(new ActionListener(){
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                modelo.setOperador1(Integer.parseInt(vista.txtOperador1.getText()));
+                modelo.setOperador2(Integer.parseInt(vista.txtOperador2.getText()));
+                modelo.sumar();
+                vista.lblResultado.setText(String.valueOf(modelo.getCalculo()));
+                
+            }
+        });
         
+        this.vista.btnLimpiar.addActionListener(new ActionListener(){
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                vista.txtOperador1.setText("");
+                vista.txtOperador2.setText("");
+                vista.lblResultado.setText("");
+                
+            }
+        });
+        
+       
     }
+    
+    
     
     public void iniciar(){
         this.vista.setVisible(true);
